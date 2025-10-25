f7aa581bbeb001ef97751642a0224a49135f6ae1
ahmed <mommentcraft@gmail.com>
2025-10-25 09:27:00 +0300

    first commit

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-export * as productsApi from './methods/products';
-export * as usersApi from './methods/users';
-export * as clientsApi from './methods/clients';
-export * as authApi from './methods/auth';
+import * as productsApi from './methods/products';
+import * as usersApi from './methods/users';
+import * as clientsApi from './methods/clients';
+import * as authApi from './methods/auth';
